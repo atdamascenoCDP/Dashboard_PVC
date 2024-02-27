54ea92088dbfc5b6b1b2a203b2d9ec18805b2039
atdamascenoCDP <160345172+atdamascenoCDP@users.noreply.github.com>
2024-02-27 01:42:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -150,11 +150,13 @@ with st.container():
     #pd.DataFrame(columns=['INDICADORES','CENÁRIO','PERÍODO','DESCRIÇÃO','DEZEMBRO 2023'])
     df_relatorio = pd.DataFrame(
     [
-        {"INDICADORES": "Quantidade de caminhões que acessam o porto", "CENÁRIO": "Setoriais", "PERÍODO": "Mensal","DESCRIÇÃO":"------", month :"DEZEMBRO 2023" },
+        {"INDICADORES": "Quantidade de caminhões que acessam o porto", "CENÁRIO": "Setoriais", "PERÍODO": "Mensal","DESCRIÇÃO":"------", month :17608 },
         {"INDICADORES": "Índice de movimentação de contêineres (vazios)", "CENÁRIO": "Setoriais", "PERÍODO": "Mensal","DESCRIÇÃO":"------", month :"DEZEMBRO 2023" },
         {"INDICADORES": "Cumprimento da programação de atracação","CENÁRIO": "Setoriais", "PERÍODO": "Mensal","DESCRIÇÃO":"------", month:"DEZEMBRO 2023" },
         {"INDICADORES": "Tempo de espera para atracação (dias)","CENÁRIO": "Setoriais", "PERÍODO": "Mensal","DESCRIÇÃO":"------", month :"DEZEMBRO 2023" },
         {"INDICADORES": "Cumprimento da programação de atracação","CENÁRIO": "Setoriais" , "PERÍODO": "Mensal","DESCRIÇÃO":"------", month :"DEZEMBRO 2023" },
+        {"INDICADORES": "Tempo de espera para atracação por berço (dias)","CENÁRIO": "Setoriais" , "PERÍODO": "Mensal","DESCRIÇÃO":"101", month :0,27 },
+        {"INDICADORES": "Tempo de espera para atracação por berço (dias)","CENÁRIO": "Setoriais" , "PERÍODO": "Mensal","DESCRIÇÃO":"102", month :0,22 },
     ]
     )
     st.dataframe(df_relatorio,2000,600,hide_index=True)
